b920a188121209cafb67a1ac0799cd0992fa5304
feliperamigon <0123321Aa>
2020-07-27 16:23:00 -0500

    Added vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,28 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Felipe Ramirez'},
+    categories: ['sustainability', 'nature', 'animal welfare', 'housing', 'education', 'food', 'community'],
+    todos: [
+      { id: 1, text: '...', done: true},
+      { id: 2, text: '...', done: false},
+      { id: 3, text: '...', done: true},
+      { id: 4, text: '...', done: false}
+    ]
+  },
   mutations: {},
   actions: {},
-  modules: {}
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length;
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done);
+    },
+    activateTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length;
+    }
+  }
 });
